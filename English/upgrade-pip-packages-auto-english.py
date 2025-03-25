7e5711fb447487c3a5744402e8fefa768b4c84bd
xystudio889 <173288240@qq.com>
2025-03-25 22:27:30 +0800

    no complete

---
+++ English/upgrade-pip-packages-auto-english.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+path = Path("requirements.txt")
+
+if os.path.exists(path):
+    os.remove(path.parent)
+
+os.system("pip list > "+str(path))
+with open(path,"r",encoding = "utf-8") as file:
+    req = file.readlines()[2:]
+
+write = []
+
+for i in req:
+    write.append(i.split(" ")[0]+"\n")
+
+with open(path,"w",encoding = "utf-8") as file:
+    file.writelines(write)
+
+os.system("python -m pip install --upgrade -r "+str(path))
+os.remove(path)
